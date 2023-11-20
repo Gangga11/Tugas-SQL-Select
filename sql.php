2128bc6eafb5d18a593ec7af8f1bd3f5c74dd350
Gangga11 <151485025+Gangga11@users.noreply.github.com>
2023-11-20 10:31:57 +0800

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020008_Gangga";
+
+    $conn    = mysqli_connect($server,$user,$password,$database);
+
+    ?>
